0d5ea0e280fdd50b6f31a37d140e61a1fa6c9aaa
Alejo Garcia <alejogarciag1@gmail.com>
2021-02-03 16:01:38 +0000

    fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ http.createServer(function (req, res) {
             return res.end;
         });
 }
-if (req.url  === "/about") {
+  if (req.url  === "/about") {
   fs.readFile('about.html',function(err,data){
             if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
@@ -19,7 +19,7 @@ if (req.url  === "/about") {
             return res.end;
         });
 }
-if (req.url  === "/contact") {
+  if (req.url  === "/contact") {
   fs.readFile('contact-me.html',function(err,data){
             if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
@@ -27,13 +27,13 @@ if (req.url  === "/contact") {
             return res.end;
         });
 }
-else {
-      fs.readFile("404.html", function(err, data) {
-        if (err) throw err;
-         res.writeHead(404, {'Content-Type': 'text/html'});
-         res.write(data)
-        return res.end("404 Not Found");
-      })  
-} 
+     else{
+        fs.readFile('404.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
 }).listen(8080);
 
