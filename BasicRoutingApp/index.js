5e61493d72afdea69f78d6d264702dd397b02df8
Alejo <alejogarb25@gmail.com>
2021-03-09 16:05:36 -0300

    Update index.js

--- BasicRoutingApp/index.js
+++ BasicRoutingApp/index.js
@@ -1,40 +1,45 @@
 var http = require('http');
-var url = require('url');
+var express = require("express")
+var app = express()
 var fs = require('fs');
+var url = require('url');
 
-http.createServer(function (req, res) {
-  if (req.url  === "/") {
-  fs.readFile('index.html',function(err,data){
-            if (err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end;
-        });
-}
- else if (req.url  === "/about") {
+const port = 8080
+
+
+app.get('/about', (req, res) => {
   fs.readFile('about.html',function(err,data){
             if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end;
         });
-}
-  else if (req.url  === "/contact") {
+});
+app.get('/contact', (req, res) => {
   fs.readFile('contact-me.html',function(err,data){
             if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end;
         });
-}
-  else {
-  fs.readFile('404.html',function(err,data){
+});
+
+app.use('/about', function (req, res, next) {
+  res.end(console.log(req.url));
+});
+
+app.get('/', (req, res) => {
+  fs.readFile('index.html',function(err,data){
             if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end;
         });
-}
+});
 
-}).listen(8080);
-
+app.use((req, res, next) => {
+  res.status(404).send("Unable to find the route you are requesting Lol.")
+})
+app.listen(port, () => {
+  console.log( "App listening on port: ", port)
+})
