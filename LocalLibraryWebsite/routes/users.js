8c1cf4e12c49b86f21d56672aaaca2e05213dad9
Alejo <alejogarb25@gmail.com>
2021-03-11 17:34:23 -0300

    Update users.js

--- LocalLibraryWebsite/routes/users.js
+++ LocalLibraryWebsite/routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  res.send('Wiki home page');
 });
+
+router.get("/about", (req, res, next) => {
+  res.send("About this wiki")
+})
 
 module.exports = router;
